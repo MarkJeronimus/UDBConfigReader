c8448aabc4e9b12b79ac2bb91daec7179473949e
MarkJeronimus <mark.jeronimus@gmail.com>
2021-08-22 17:15:31 +0200

    Parse include files using same instance as parent

--- src/org/digitalmodular/udbconfigreader/GameConfigurationIO.java
+++ src/org/digitalmodular/udbconfigreader/GameConfigurationIO.java
@@ -1,22 +1,7 @@
 package org.digitalmodular.udbconfigreader;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Logger;
-import static java.util.logging.Level.FINER;
-import static java.util.logging.Level.INFO;
-
-import org.jetbrains.annotations.Nullable;
-
-import org.digitalmodular.udbconfigreader.lexer.CleaningLexer;
-import org.digitalmodular.udbconfigreader.lexer.CommentsLexer;
-import org.digitalmodular.udbconfigreader.lexer.ConfigToken;
-import org.digitalmodular.udbconfigreader.lexer.ConfigTokenizer;
-import org.digitalmodular.udbconfigreader.lexer.KeywordLexer;
-import org.digitalmodular.udbconfigreader.lexer.StringsLexer;
 
 /**
  * @author Zom-B
@@ -31,55 +16,8 @@ public final class GameConfigurationIO {
 	 * Load a configuration file as a {@code ConfigStruct} structure.
 	 */
 	public static ConfigStruct loadGameConfiguration(Path file) throws IOException {
-		List<Path> fileStack = new ArrayList<>(4);
-		fileStack.add(file);
-		return loadConfigurationFile(fileStack);
-	}
+		RecursiveConfigFileLoader parser = new RecursiveConfigFileLoader();
 
-	/**
-	 * Internal method to load configuration files recursively, via
-	 *
-	 * @param fileStack The list of files which are currently being parsed.
-	 *                  Each subsequent element is a file included by the previous element.
-	 *                  The last element is the one to parse next.
-	 */
-	static ConfigStruct loadConfigurationFile(List<Path> fileStack) throws IOException {
-		assert !fileStack.isEmpty();
-		Path file = fileStack.get(fileStack.size() - 1);
-
-		@Nullable ConfigStruct gameConfiguration = ConfigFileCache.INSTANCE.get(file);
-		if (gameConfiguration != null)
-			return gameConfiguration;
-
-		if (Logger.getGlobal().isLoggable(FINER))
-			Logger.getGlobal().log(INFO, "Loading configuration file: " + file.toAbsolutePath());
-		else
-			Logger.getGlobal().log(INFO, "Loading configuration file: " + file);
-
-		try {
-			List<String>    lines  = Files.readAllLines(file);
-			CharacterReader reader = new CharacterReader(file.getFileName().toString(), lines);
-
-			gameConfiguration = new ConfigStruct(file.toString(), 16);
-
-			parseConfiguration(fileStack, reader, gameConfiguration);
-
-			ConfigFileCache.INSTANCE.add(file, gameConfiguration);
-			return gameConfiguration;
-		} catch (IOException ex) {
-			throw new IOException("Unable to load the game configuration file: " + file, ex);
-		}
-	}
-
-	private static void parseConfiguration(
-			List<Path> fileStack, CharacterReader reader, ConfigStruct gameConfiguration) {
-		List<ConfigToken> tokens = ConfigTokenizer.tokenize(reader);
-
-		tokens = StringsLexer.process(tokens);
-		tokens = CommentsLexer.process(tokens);
-		tokens = KeywordLexer.process(tokens);
-		tokens = CleaningLexer.process(tokens);
-
-		ConfigParser.parse(fileStack, tokens, gameConfiguration);
+		return parser.loadConfigurationFile(file);
 	}
 }
--- src/org/digitalmodular/udbconfigreader/RecursiveConfigFileLoader.java
+++ src/org/digitalmodular/udbconfigreader/RecursiveConfigFileLoader.java
@@ -1,18 +1,27 @@
 package org.digitalmodular.udbconfigreader;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Logger;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import static java.util.logging.Level.FINER;
+import static java.util.logging.Level.INFO;
 
 import org.jetbrains.annotations.Nullable;
 
-import org.digitalmodular.utilities.annotation.UtilityClass;
-
+import org.digitalmodular.udbconfigreader.lexer.CleaningLexer;
+import org.digitalmodular.udbconfigreader.lexer.CommentsLexer;
 import org.digitalmodular.udbconfigreader.lexer.ConfigToken;
+import org.digitalmodular.udbconfigreader.lexer.ConfigTokenizer;
+import org.digitalmodular.udbconfigreader.lexer.KeywordLexer;
+import org.digitalmodular.udbconfigreader.lexer.StringsLexer;
 import static org.digitalmodular.udbconfigreader.lexer.ConfigToken.TokenType;
 import static org.digitalmodular.udbconfigreader.lexer.ConfigToken.TokenType.STATEMENT_SEPARATOR;
 
@@ -23,77 +32,118 @@ import static org.digitalmodular.udbconfigreader.lexer.ConfigToken.TokenType.STA
  * @author Zom-B
  */
 // Created 2021-08-13
-@UtilityClass
-public final class ConfigParser {
-	private static final Pattern BACKSLASH_PATTERN = Pattern.compile("\\\\");
-
-	private ConfigParser() {
-		throw new AssertionError();
-	}
+public class RecursiveConfigFileLoader {
+	private final Pattern backslashPattern = Pattern.compile("\\\\");
 
 	/**
-	 * @param fileStack The list of files which are currently being parsed.
-	 *                  Each subsequent element is a file included by the previous element.
-	 *                  The last element is the one being parsed.
-	 *                  This is used exclusively by the {@code include()} function, to detect loops.
+	 * The list of files which are currently being parsed.
+	 * Each subsequent element is a file included by the previous element.
+	 * The last element is the one currently being parsed.
 	 */
-	public static void parse(List<Path> fileStack, Iterable<ConfigToken> tokens, ConfigStruct destination) {
-		Iterator<ConfigToken> iter = tokens.iterator();
+	private final Deque<Path>         fileStack         = new ArrayDeque<>(8);
+	private final Deque<ConfigStruct> configStructStack = new ArrayDeque<>(8);
 
-		while (iter.hasNext()) {
-			ConfigToken token = iter.next();
-			switch (token.getTokenType()) {
-				case STATEMENT_SEPARATOR:
-					continue;
-				case OTHER:
-					parseEntry(fileStack, token, iter, destination);
-					break;
-				default:
-					throwSyntaxError(token, "identifier");
-			}
+	public ConfigStruct loadConfigurationFile(Path file) throws IOException {
+		@Nullable ConfigStruct gameConfiguration = ConfigFileCache.INSTANCE.get(file);
+		if (gameConfiguration != null)
+			return gameConfiguration;
+
+		if (Logger.getGlobal().isLoggable(FINER))
+			Logger.getGlobal().log(INFO, "Loading configuration file: " + file.toAbsolutePath());
+		else
+			Logger.getGlobal().log(INFO, "Loading configuration file: " + file);
+
+		gameConfiguration = new ConfigStruct(file.toString(), 16);
+
+		fileStack.push(file);
+		configStructStack.push(gameConfiguration);
+		try {
+			parseConfigurationFile(file);
+		} finally {
+			fileStack.pop();
+			configStructStack.pop();
+		}
+
+		ConfigFileCache.INSTANCE.add(file, gameConfiguration);
+		return gameConfiguration;
+	}
+
+	private void parseConfigurationFile(Path file) throws IOException {
+		try {
+			List<String> lines = Files.readAllLines(file);
+
+			CharacterReader reader = new CharacterReader(file.getFileName().toString(), lines);
+
+			List<ConfigToken> tokens = ConfigTokenizer.tokenize(reader);
+
+			tokens = StringsLexer.process(tokens);
+			tokens = CommentsLexer.process(tokens);
+			tokens = KeywordLexer.process(tokens);
+			tokens = CleaningLexer.process(tokens);
+
+			parseTokens(tokens.iterator());
+		} catch (IOException ex) {
+			throw new IOException("Unable to load the game configuration file: " + file, ex);
 		}
 	}
 
-	private static void parseEntry(
-			List<Path> fileStack, ConfigToken firstToken, Iterator<ConfigToken> iter, ConfigStruct destination) {
-		ConfigToken token = iter.next();
+	public void parseTokens(Iterator<ConfigToken> tokens) {
+		while (tokens.hasNext())
+			parseStatement(tokens);
+	}
+
+	private void parseStatement(Iterator<ConfigToken> tokens) {
+		ConfigToken firstToken = tokens.next();
+
+		switch (firstToken.getTokenType()) {
+			case STATEMENT_SEPARATOR:
+				return;
+			case OTHER:
+				parseEntry(firstToken, tokens);
+				break;
+			default:
+				throwSyntaxError(firstToken, "identifier");
+		}
+	}
+
+	private void parseEntry(ConfigToken firstToken, Iterator<ConfigToken> tokens) {
+		ConfigToken token = tokens.next();
 
 		switch (token.getTokenType()) {
 			case ASSIGNMENT:
-				parseAssignment(firstToken, iter, destination);
+				parseAssignment(firstToken, tokens);
 				return;
 			case FUNCTION_START:
-				parseFunction(fileStack, firstToken, iter, destination);
+				parseFunction(firstToken, tokens);
 				return;
 			case BLOCK_START:
-				parseBlock(fileStack, firstToken, iter, destination);
+				parseBlock(firstToken, tokens);
 				return;
 			case STATEMENT_SEPARATOR:
-				destination.put(firstToken.getText(), null);
+				configStructStack.getFirst().put(firstToken.getText(), null);
 				return;
 			default:
 				throwSyntaxError(token, "\"=\", \"{\", or \"(\" after an identifier");
 		}
 	}
 
-	private static void parseAssignment(
-			ConfigToken firstToken, Iterator<ConfigToken> iter, ConfigStruct destination) {
+	private void parseAssignment(ConfigToken firstToken, Iterator<ConfigToken> iter) {
 		requireHasNextToken(iter, firstToken, "a variable assignment");
 		ConfigToken token = iter.next();
 
 		switch (token.getTokenType()) {
 			case STRING:
-				destination.put(firstToken.getText(), token.getText());
+				configStructStack.getFirst().put(firstToken.getText(), token.getText());
 				break;
 			case OTHER:
 				if (token.getText().equalsIgnoreCase("null")) {
-					destination.put(firstToken.getText(), null);
+					configStructStack.getFirst().put(firstToken.getText(), null);
 					break;
 				}
 
 				@Nullable Object value = parseValue(token.getText());
 				if (value != null) {
-					destination.put(firstToken.getText(), value);
+					configStructStack.getFirst().put(firstToken.getText(), value);
 					break;
 				}
 
@@ -134,8 +184,7 @@ public final class ConfigParser {
 		return null;
 	}
 
-	private static void parseFunction(
-			List<Path> fileStack, ConfigToken firstToken, Iterator<ConfigToken> iter, ConfigStruct destination) {
+	private void parseFunction(ConfigToken firstToken, Iterator<ConfigToken> iter) {
 		List<Object> parameters = new ArrayList<>(8);
 
 		boolean     requireValue = true;
@@ -158,7 +207,7 @@ public final class ConfigParser {
 
 					break;
 				case FUNCTION_END:
-					callFunction(fileStack, firstToken, parameters, destination);
+					callFunction(firstToken, parameters);
 					return;
 				default:
 					throwSyntaxError(token, "nothing");
@@ -168,20 +217,18 @@ public final class ConfigParser {
 		}
 	}
 
-	private static void callFunction(
-			List<Path> fileStack, ConfigToken firstToken, List<Object> parameters, ConfigStruct destination) {
+	private void callFunction(ConfigToken firstToken, List<Object> parameters) {
 		String function = firstToken.getText();
 		assert !function.isEmpty();
 		if ("include".equals(function.toLowerCase())) {
-			callIncludeFunction(fileStack, firstToken, parameters, destination);
+			callIncludeFunction(firstToken, parameters);
 		} else {
 			throw new IllegalArgumentException("Unknown function: " + function + ", at " +
 			                                   firstToken.getLocationString());
 		}
 	}
 
-	private static void callIncludeFunction(
-			List<Path> fileStack, ConfigToken firstToken, List<Object> parameters, ConfigStruct destination) {
+	private void callIncludeFunction(ConfigToken firstToken, List<Object> parameters) {
 		if (parameters.isEmpty())
 			throw new IllegalArgumentException("include() is missing parameters, at " + firstToken.getLocationString());
 
@@ -195,10 +242,10 @@ public final class ConfigParser {
 			throw new IllegalArgumentException("Second parameter of include() must be a string, at " +
 			                                   firstToken.getLocationString());
 
-		String filenameString = BACKSLASH_PATTERN.matcher(((String)filename)).replaceAll("/");
+		String filenameString = backslashPattern.matcher((String)filename).replaceAll("/");
 
 		assert !fileStack.isEmpty();
-		Path originalFile = fileStack.get(fileStack.size() - 1);
+		Path originalFile = fileStack.getFirst();
 		Path includeFile  = originalFile.getParent().resolve(filenameString);
 
 		if (fileStack.contains(includeFile)) {
@@ -210,10 +257,8 @@ public final class ConfigParser {
 			                                   ", at " + firstToken.getLocationString());
 		}
 
-		fileStack.add(includeFile);
-
 		try {
-			ConfigStruct block = GameConfigurationIO.loadConfigurationFile(fileStack);
+			ConfigStruct block = loadConfigurationFile(includeFile);
 
 			String sectionName = (String)section;
 			if (!sectionName.isEmpty()) {
@@ -230,35 +275,37 @@ public final class ConfigParser {
 				}
 			}
 
-			destination.putAll(block);
+			configStructStack.getFirst().putAll(block);
 		} catch (IOException ex) {
 			throw new IllegalArgumentException("Unable to read include file: " + filename, ex);
-		} finally {
-			fileStack.remove(fileStack.size() - 1);
 		}
 	}
 
-	private static void parseBlock(
-			List<Path> fileStack, ConfigToken firstToken, Iterator<ConfigToken> iter, ConfigStruct destination) {
+	private void parseBlock(ConfigToken firstToken, Iterator<ConfigToken> iter) {
 		requireHasNextToken(iter, firstToken, "a block");
 
 		ConfigStruct block = new ConfigStruct(firstToken.getText(), 16);
 
-		ConfigToken token;
-		while (true) {
-			token = iter.next();
-			switch (token.getTokenType()) {
-				case STATEMENT_SEPARATOR:
-					continue;
-				case OTHER:
-					parseEntry(fileStack, token, iter, block);
-					break;
-				case BLOCK_END:
-					destination.put(firstToken.getText(), block);
-					return;
-				default:
-					throwSyntaxError(token, "identifier");
+		configStructStack.push(block);
+		try {
+			ConfigToken token;
+			while (true) {
+				token = iter.next();
+				switch (token.getTokenType()) {
+					case STATEMENT_SEPARATOR:
+						continue;
+					case OTHER:
+						parseEntry(token, iter);
+						break;
+					case BLOCK_END:
+						return;
+					default:
+						throwSyntaxError(token, "identifier");
+				}
 			}
+		} finally {
+			configStructStack.pop();
+			configStructStack.getFirst().put(firstToken.getText(), block);
 		}
 	}
 
